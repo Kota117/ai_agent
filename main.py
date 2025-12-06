dc87e0aac8b819488762ddac9fb696d66af6f23a
Kota117 <dakotah1375@gmail.com>
2025-12-05 19:42:03 -0700

    Load API key from file

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+api_key_name = "GEMINI_API_KEY"
+
 def main():
-    print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get(api_key_name)
+    if api_key is None:
+        raise RuntimeError(f"API key not found! Is there a '.env' file with a value for the '{api_key_name}' key?")
 
 
 if __name__ == "__main__":

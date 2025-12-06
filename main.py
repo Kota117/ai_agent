354b84b8e6c07ab2f8ae38f470dff66e82a82815
Kota117 <dakotah1375@gmail.com>
2025-12-05 19:55:34 -0700

    Generate response

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from google import genai
 
 api_key_name = "GEMINI_API_KEY"
 
@@ -8,6 +9,14 @@ def main():
     api_key = os.environ.get(api_key_name)
     if api_key is None:
         raise RuntimeError(f"API key not found! Is there a '.env' file with a value for the '{api_key_name}' key?")
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    print(response.text)
 
 
 if __name__ == "__main__":

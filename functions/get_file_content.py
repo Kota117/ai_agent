fcc33c64d28cfe93a9d30071dc419b1152663d60
Kota117 <dakotah1375@gmail.com>
2025-12-11 19:24:26 -0700

    Updated syntax to more closely match the solution files

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -11,20 +11,20 @@ except ModuleNotFoundError:
 
 def get_file_content(working_directory, file_path):
     abs_working_dir = os.path.abspath(working_directory)
-    target_file = os.path.abspath(os.path.join(working_directory, file_path))
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
 
-    if not target_file.startswith(abs_working_dir):
+    if not abs_file_path.startswith(abs_working_dir):
         return f"Error: Cannot read \"{file_path}\" as it is outside the permitted working directory"
     
-    if not os.path.isfile(target_file):
+    if not os.path.isfile(abs_file_path):
         return f"Error: File not found or is not a regular file: \"{file_path}\""
 
     try:
-        with open(target_file, 'r') as inFile:
+        with open(abs_file_path, 'r') as inFile:
             file_content_string = inFile.read(MAX_CHARS)
 
-            if len(file_content_string) >= MAX_CHARS:
-                file_content_string += f"[...File \"{target_file}\" truncated at 10000 characters]"
+            if len(file_content_string) > MAX_CHARS:
+                file_content_string += f"[...File \"{file_path}\" truncated at 10000 characters]"
             return file_content_string
     except Exception as e:
-        return f"Error listing file contents: {e}"
+        return f"Error reading file \"{file_path}\": {e}"

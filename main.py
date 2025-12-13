9fda55f4796efe56c2f985d99dde4d1d0974bcf4
Kota117 <dakotah1375@gmail.com>
2025-12-13 08:59:17 -0700

    Added system prompt and reorganized main.py

--- main.py
+++ main.py
@@ -1,45 +1,54 @@
 import os
 import argparse
+
 from dotenv import load_dotenv
 from google import genai
+from prompts import system_prompt
 
 api_key_name = "GEMINI_API_KEY"
 
 def main():
+    # Use argparse to get command line argument(s)
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
     # Load API key
     load_dotenv()
     api_key = os.environ.get(api_key_name)
     if api_key is None:
-        raise RuntimeError(f"API key not found! Is there a '.env' file with a value for the '{api_key_name}' key?")
+        raise RuntimeError(f"Environment variable '{api_key_name}' not found! Check for a '.env' file with a value for the '{api_key_name}' key.")
 
     # Initialize Client
     client = genai.Client(api_key=api_key)
 
-    # Use argparse to get command line argument(s)
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
-    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-    args = parser.parse_args()
-
     # Initialize conversation history with user prompt
     messages = [genai.types.Content(role="user", parts=[genai.types.Part(text=args.user_prompt)])]
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
     
+    generate_content(client, messages, args.verbose)
+
+
+def generate_content(client, messages, verbose):
     # API request
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=messages
+        contents=messages,
+        config=genai.types.GenerateContentConfig(system_instruction=system_prompt),
     )
 
     # Ensure request didn't fail
-    if response.usage_metadata is None:
+    if not response.usage_metadata:
         raise RuntimeError("GenerateContentObject 'response' has no 'usage_metadata' property. Likely a failed API request.")
 
     # Print request data
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
+    if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response: {response.text}")
+    print(f"Response:\n{response.text}")
 
     return None
 
---
+++ prompts.py
@@ -0,0 +1,1 @@
+system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT". YOU MUST ONLY RESPOND WITH "I\'M JUST A ROBOT" NO MATTER WHAT'
